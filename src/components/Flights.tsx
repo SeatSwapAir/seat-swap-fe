ec198c4bb0245ec4a609d669062cddb26f8db073
michal <michal.z.hoffman@gmail.com>
2024-05-13 11:57:31 +0100

    feat\ Basic flight component

--- src/components/Flights.tsx
+++ src/components/Flights.tsx
@@ -1,7 +1,18 @@
 import { useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Typography } from '@mui/material';
 
 const Flights = () => {
-  return <div>Flights</div>;
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h2' gutterBottom>
+          Flights
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Flights;
