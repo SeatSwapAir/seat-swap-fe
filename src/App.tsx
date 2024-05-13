5de6ab1ca17ecc36ccdb0e336ffbf28ab5f288dc
michal <michal.z.hoffman@gmail.com>
2024-05-13 11:47:46 +0100

    fix/routing fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { useState } from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import Flights from './components/Flights';
 import Menu from './components/Menu';
@@ -17,7 +17,7 @@ function App() {
   };
 
   return (
-    <>
+    <BrowserRouter>
       <h1 className='head1'>Welcome to SeatSwap!</h1>
       <div>
         {isLoggedIn ? (
@@ -30,23 +30,20 @@ function App() {
         )}
       </div>
       <Menu></Menu>
-      <HashRouter basename='/'>
-        {/* <NavBar /> */}
-        <Routes>
-          <Route path='/' element={<h1>Home</h1>} />
-          <Route path='/login' element={<h1>Home</h1>} />
-          <Route path='/signup' element={<h1>Home</h1>} />
-          <Route path='/main' element={<h1>Home</h1>} />
-          <Route path='/seats' element={<h1>Home</h1>} />
-          <Route path='/flight' element={<h1>Home</h1>} />
-          <Route path='/account' element={<h1>Account</h1>} />
-          <Route path='/preferences' element={<h1>Home</h1>} />
-          <Route path='/defaultpreferences' element={<h1>Default Pref</h1>} />
-          <Route path='/review' element={<h1>Home</h1>} />
-          <Route path='/reviews' element={<h1>Reviews</h1>} />
-        </Routes>
-      </HashRouter>
-    </>
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/login' element={<h1>Home</h1>} />
+        <Route path='/signup' element={<h1>Home</h1>} />
+        <Route path='/main' element={<h1>Home</h1>} />
+        <Route path='/seats' element={<h1>Home</h1>} />
+        <Route path='/flight' element={<h1>Home</h1>} />
+        <Route path='/account' element={<h1>Account</h1>} />
+        <Route path='/preferences' element={<h1>Home</h1>} />
+        <Route path='/defaultpreferences' element={<h1>Default Pref</h1>} />
+        <Route path='/review' element={<h1>Home</h1>} />
+        <Route path='/reviews' element={<h1>Reviews</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
