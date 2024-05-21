322770b9aecf35e4cb668938057c485a9afd299f
michal <michal.z.hoffman@gmail.com>
2024-05-21 11:40:31 +0100

    fix: redirect after log out

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { useState } from 'react';
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Outlet, Route } from 'react-router-dom';
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 
 import Menu from './components/Menu';
 import Home from './components/Home';
-
-
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
@@ -15,34 +19,38 @@ function App() {
     setIsLoggedIn(true);
   };
 
-
   const handleLogout: React.MouseEventHandler<HTMLAnchorElement> = (event) => {
+    const navigate = useNavigate();
     event.preventDefault();
+    navigate('/');
     setIsLoggedIn(false);
   };
 
   const AppLayout = () => (
     <>
-    <Menu loggedInStatus={isLoggedIn} handleLogout={handleLogout}/>
-    <Outlet />
+      <Menu loggedInStatus={isLoggedIn} handleLogout={handleLogout} />
+      <Outlet />
     </>
-  )
-
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<AppLayout />}>
-      <Route index element={<Home handleLogin={handleLogin} isLoggedIn={isLoggedIn}/>}/>
-      <Route path='/login' element={<h1>Home</h1>} />
-      <Route path='/signup' element={<h1>Home</h1>} />
-      <Route path='/account' element={<h1>Account</h1>} />
-      <Route path='/defaultpreferences' element={<h1>Default Pref</h1>} />
-      <Route path='/review' element={<h1>Home</h1>} />
-      <Route path='/reviews' element={<h1>Reviews</h1>} />
-    </Route>
-  ))
-
-  return (
-    <RouterProvider router={router}/>
   );
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<AppLayout />}>
+        <Route
+          index
+          element={<Home handleLogin={handleLogin} isLoggedIn={isLoggedIn} />}
+        />
+        <Route path='/login' element={<h1>Home</h1>} />
+        <Route path='/signup' element={<h1>Home</h1>} />
+        <Route path='/account' element={<h1>Account</h1>} />
+        <Route path='/defaultpreferences' element={<h1>Default Pref</h1>} />
+        <Route path='/review' element={<h1>Home</h1>} />
+        <Route path='/reviews' element={<h1>Reviews</h1>} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
