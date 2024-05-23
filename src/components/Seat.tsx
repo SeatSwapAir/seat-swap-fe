f4d15fafa2a9a99a0f7fa1ed6f1bf0d338176597
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2024-05-23 12:48:51 +0100

    feat: Seat forms

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -4,6 +4,18 @@ import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import Switch from '@mui/material/Switch';
+
+
 
 import { Seat as SeatProps } from '../../lib/types';
 import { FlightCardProps } from '../../lib/types';
@@ -16,6 +28,108 @@ export default function Seat({
   handleDelete: FlightCardProps['handleDelete'];
 }) {
   const [showEditSeat, setShowEditSeat] = useState(false);
+  const [rowNumber, setRowNumber] = useState('');
+  const [seatLetter, setSeatLetter] = useState('');
+  const [location, setLocation] = useState('');
+  const [position, setPosition] = useState('');
+  const [legroom, setLegroom] = useState(false);
+
+
+  const rows: Number[] = [];
+
+  const letters: string[] = ['A','B','C','D','E','F','G','H','I','J']
+
+  for (let i=0; i<66; i++) {
+    rows.push(i);
+  };
+
+  const doRowNumber = (event: SelectChangeEvent<string>) => {
+    setRowNumber(event.target.value);
+  }
+
+  const doSeatLetter = (event: SelectChangeEvent<string>) => {
+    setSeatLetter(event.target.value);
+  }
+
+  const doLocation = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLocation(event.target.value)
+  }
+
+  const doPosition = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPosition(event.target.value)
+  }
+
+  const toggleLegroom = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLegroom(event.target.checked);
+  };
+
+  const seatForm = () => {
+    return <>
+      <Typography variant='body2'>
+      <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Row Number</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={rowNumber}
+          label="Row Number"
+          onChange={doRowNumber}
+        >
+          {rows.map((row) => (
+    <MenuItem key={row.toString()} value={row.toString()}>{row.toString()}</MenuItem>
+    ))}   
+        </Select>
+      </FormControl>
+    </Box>
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Seat Letter</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={seatLetter}
+          label="Seat Letter"
+          onChange={doSeatLetter}
+        >
+          {letters.map((letter) => (
+    <MenuItem key={letter} value={letter}>{letter}</MenuItem>
+    ))}   
+        </Select>
+      </FormControl>
+    </Box>
+    <FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">Location</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        value={location}
+        onChange={doLocation}
+      >
+        <FormControlLabel value="front" control={<Radio />} label="Front of plane" />
+        <FormControlLabel value="middle" control={<Radio />} label="Middle of plane" />
+        <FormControlLabel value="back" control={<Radio />} label="Back of plane" />
+      </RadioGroup>
+    </FormControl>
+    <FormControl>
+      <FormLabel id="demo-controlled-radio-buttons-group">Seat Position</FormLabel>
+      <RadioGroup
+        aria-labelledby="demo-controlled-radio-buttons-group"
+        name="controlled-radio-buttons-group"
+        value={position}
+        onChange={doPosition}
+      >
+        <FormControlLabel value="window" control={<Radio />} label="Window" />
+        <FormControlLabel value="middle" control={<Radio />} label="Middle" />
+        <FormControlLabel value="aisle" control={<Radio />} label="Aisle" />
+      </RadioGroup>
+    </FormControl>
+    <FormControlLabel control={<Switch checked={legroom}
+      onChange={toggleLegroom}/>} label="This seat has extra legroom" />
+      </Typography>
+    </>
+}
+
   return (
     <>
       <Typography variant='body2'>
@@ -38,7 +152,9 @@ export default function Seat({
           Edit Seats
         </Button>
       </Typography>
-      {showEditSeat && 'Edit seat here'}
+      {showEditSeat && (
+        seatForm()
+      )}
     </>
   );
 }
