adad621ae44936995b7ca2de80db14bb9f171d2b
michal <michal.z.hoffman@gmail.com>
2024-05-28 13:26:31 +0100

    feat: rating and welcome message

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,40 +1,42 @@
 import { useState } from 'react';
-import { Typography } from '@mui/material';
+import { Typography, Rating } from '@mui/material';
 
-
-import {User} from '../../lib/types';
+import { User } from '../../lib/types';
 import ChangePassword from './ChangePassword';
 import ChangeContactDetails from './ChangeContactDetails';
 
 const mockUser: User = {
-    email: 'testuser@gmail.com',
-    firstname: 'Bob',
-    lastname: 'Marley',
-    gender: 1,
-    phone: 123456789,
-    rating: 4.2,
-    created_at: '1979-04-02',
-      }
+  email: 'testuser@gmail.com',
+  firstname: 'Bob',
+  lastname: 'Marley',
+  gender: 1,
+  phone: 123456789,
+  rating: 4.2,
+  created_at: '1979-04-02',
+};
 
 export default function Account() {
-    const [user, setUser] = useState(mockUser);
+  const [user, setUser] = useState(mockUser);
 
-    const   {email,
-            firstname,
-            lastname,
-            gender,
-            phone,
-            rating,
-            created_at} = user
-   
-    return (
-        <Typography variant='h2'>
-            <ChangePassword/>
-            <ChangeContactDetails email = {email}
-            firstname = {firstname}
-            lastname = {lastname}
-            gender = {gender}
-            phone = {phone}/>
-        </Typography>
-    )
+  const { email, firstname, lastname, gender, phone, rating, created_at } =
+    user;
+
+  return (
+    <Typography variant='body1'>
+      <Typography variant='h4'>Account Settings</Typography>
+      <Typography variant='h6'>
+        Hello {firstname} {lastname}!
+      </Typography>
+      <Typography component='legend'>Your rating is:</Typography>
+      <Rating name='read-only' value={rating} readOnly />
+      <ChangePassword />
+      <ChangeContactDetails
+        email={email}
+        firstname={firstname}
+        lastname={lastname}
+        gender={gender}
+        phone={phone}
+      />
+    </Typography>
+  );
 }
