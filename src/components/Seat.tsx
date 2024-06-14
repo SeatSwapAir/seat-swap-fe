a175d442affac74ae030b6d91e9e56152a6e5916
michal <michal.z.hoffman@gmail.com>
2024-06-14 13:16:01 +0100

    refactor: Removing useless loging and buttons

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -1,28 +1,17 @@
-import { Typography, Button } from '@mui/material';
-import { Delete } from '@mui/icons-material';
+import { Typography } from '@mui/material';
 
 import { Seat as SeatProps } from '../../lib/types';
 
 export default function Seat({
   seat,
-  handleDelete,
 }: {
   seat: SeatProps;
   flightNumber: string;
-  handleDelete: (id: string) => void;
 }) {
   return (
     <Typography variant='body2'>
       Seat {seat.number} - {seat.location} - {seat.position} -{' '}
       {seat.extraLegroom ? 'Extra Legroom' : 'Standard Legroom'}
-      <Button
-        variant='contained'
-        color='primary'
-        onClick={() => handleDelete(seat.id)}
-        value={seat.number}
-      >
-        <Delete />
-      </Button>
     </Typography>
   );
 }
