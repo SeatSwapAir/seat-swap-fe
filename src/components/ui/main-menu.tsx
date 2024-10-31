4743058ae5fe282fa3110202e33dc225957deb86
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2024-10-31 16:08:04 +0000

    feat: logout hooked up to auth0

--- src/components/ui/main-menu.tsx
+++ src/components/ui/main-menu.tsx
@@ -1,9 +1,16 @@
 import { useMediaQuery } from '@/hooks/use-media-query';
 import { Drawer, DrawerTrigger, DrawerContent } from '@/components/ui/drawer';
 import { MenuIcon } from 'lucide-react';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function MainMenu() {
   const isDesktop = useMediaQuery('(min-width: 768px)');
+  const { logout } = useAuth0();
+
+  const handleLogout = () => {
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
   return isDesktop ? (
     <div className=''>
       <ul className='flex flex-row gap-4 p-4'>
@@ -11,7 +18,9 @@ export default function MainMenu() {
         <li>Your Journeys</li>
         <li>Add Journey</li>
         <li>Reviews</li>
-        <li>Logout</li>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
+        </li>
       </ul>
     </div>
   ) : (
@@ -26,7 +35,9 @@ export default function MainMenu() {
             <li>Your Journeys</li>
             <li>Add Journey</li>
             <li>Reviews</li>
-            <li>Logout</li>
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>
           </ul>
         </DrawerContent>
       </Drawer>
