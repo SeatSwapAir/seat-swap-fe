278c624f1bd493b7fd16b47246b8ac99a81a9a6f
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-11-01 15:06:15 +0000

    test: test console logs for troubleshooting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
+console.log(import.meta.env.VITE_APP_AUTH0_DOMAIN);
+console.log(import.meta.env.VITE_APP_AUTH0_CLIENT_ID);
+console.log(import.meta.env.VITE_APP_AUTH0_AUDIENCE);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
