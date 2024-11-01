bd47d2dac51e64835d115f8e45463aadce99504c
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-11-01 15:12:04 +0000

    xxxx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-console.log(import.meta.env.VITE_APP_AUTH0_DOMAIN);
-console.log(import.meta.env.VITE_APP_AUTH0_CLIENT_ID);
-console.log(import.meta.env.VITE_APP_AUTH0_AUDIENCE);
+console.log(import.meta.env.VITE_AUTH0_DOMAIN);
+console.log(import.meta.env.VITE_AUTH0_CLIENT_ID);
+console.log(import.meta.env.VITE_AUTH0_AUDIENCE);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
