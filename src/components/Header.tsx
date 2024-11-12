0e08b17b8521b2c11dcc6760c9b5dcb427b5f9d3
Michal Hoffman <michal.z.hoffman@gmail.com>
2024-11-12 14:39:22 +0000

    fix: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import SeedPrompt from './SeedPrompt';
 import MainMenu from './ui/main-menu';
+import Logo from '../../public/SeatSwapIconMono.png';
 
 const Header = () => {
   return (
@@ -8,11 +9,7 @@ const Header = () => {
       <div className=' border-b-2 border-cyan-600 h-[60px] bg-background flex flex-row justify-between items-center p-3'>
         <Link to={`/`} style={{ color: 'inherit', textDecoration: 'none' }}>
           <div className='font-bold text-xl flex align-middle items-center'>
-            <img
-              src='../SeatSwapIconMono.png'
-              alt='logo'
-              className='h-10 w-10 mr-2'
-            />{' '}
+            <img src={Logo} alt='logo' className='h-10 w-10 mr-2' />{' '}
             <span>SeatSwap</span>
           </div>
         </Link>
