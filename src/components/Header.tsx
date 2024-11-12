a3f41ee93c85dad241718f7dceecd64818094800
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2024-11-12 11:06:30 +0000

    feat: making the link for the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import SeedPrompt from './SeedPrompt';
 import MainMenu from './ui/main-menu';
 
@@ -5,7 +6,9 @@ const Header = () => {
   return (
     <>
       <div className=' border-b-2 border-cyan-600 h-[60px] bg-background flex flex-row justify-between items-center p-3'>
-        <span className='font-bold text-xl'>SeatSwap</span>
+        <Link to={`/`} style={{ color: 'inherit', textDecoration: 'none' }}>
+          <span className='font-bold text-xl'>SeatSwap</span>
+        </Link>
         <MainMenu />
       </div>
       <SeedPrompt />
